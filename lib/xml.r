bf505de1dc6659aa2af02fc168d8edd2fc1953e5
Spela Povrzenic <spela.povrzenic@gmail.com>
2014-11-25 22:15:41 +0100

    nov link

--- lib/xml.r
+++ lib/xml.r
@@ -10,7 +10,7 @@ stripByPath <- function(x, path) {
 }
 
 uvozi.obcine <- function() {
-  url.obcine <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+  url.obcine <- "http://epp.eurostat.ec.europa.eu/cache/ITY_FIXDST/ten00006.xml"
   doc.obcine <- htmlTreeParse(url.obcine, useInternalNodes=TRUE)
   
   # Poiščemo vse tabele v dokumentu
