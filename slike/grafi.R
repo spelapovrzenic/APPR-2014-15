d16002a0a12def7f9f567bf0bbe6594088ad80bb
Spela Povrzenic <spela.povrzenic@gmail.com>
2014-12-02 21:50:16 +0100

    popravek

--- slike/grafi.R
+++ slike/grafi.R
@@ -4,6 +4,6 @@ plot(porecja[,12], xlab="porečja", ylab="količina dobavljene vode",
      main="Porečja, leto 2013",pch=20, col="blue")
 
 barplot(as.matrix(stopnje[1:10, 4]), beside=TRUE, legend.text = rownames(stopnje)[1:10],
-        main="Količina arzenika v vodovodnih sistemih", col="red")
+        main="Količina arzenika v vodovodnih sistemih")
 
 dev.off()
