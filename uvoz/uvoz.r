db44cf3cbce2923aed8f85b1a35f5f7ee92260b5
Spela Povrzenic <spela.povrzenic@gmail.com>
2014-12-03 23:27:57 +0100

    uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,23 +1,5 @@
 # 2. faza: Uvoz podatkov
 
-# # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-# uvoziDruzine <- function() {
-#   return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-#                       row.names = 1,
-#                       col.names = c("obcina", "en", "dva", "tri", "stiri"),
-#                       fileEncoding = "Windows-1250"))plot
-# }
-# 
-# # Zapišimo podatke v razpredelnico druzine.
-# cat("Uvažam podatke o družinah...\n")
-# druzine <- uvoziDruzine()
-
-# Če bi imeli  več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
-
 #TABELA 1
 uvozi1 <- function() {
   return(read.csv("podatki/1tabela.csv", sep = ";", as.is = TRUE,
