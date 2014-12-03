3891cd96c3188ffacda072d281765998697ac941
Spela Povrzenic <spela.povrzenic@gmail.com>
2014-12-03 10:52:18 +0100

    k

--- projekt.r
+++ projekt.r
@@ -1,10 +1,11 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
 source("uvoz/uvoz.r")
+source("slike/grafi.r")
 
-# 3. faza: Analiza in vizualizacija podatkov
-source("vizualizacija/vizualizacija.r")
-
-# 4. faza: Napredna analiza podatkov
-source("analiza/analiza.r")
+# # 3. faza: Analiza in vizualizacija podatkov
+# source("vizualizacija/vizualizacija.r")
+# 
+# # 4. faza: Napredna analiza podatkov
+# source("analiza/analiza.r")
 
 cat("Končano.\n")
