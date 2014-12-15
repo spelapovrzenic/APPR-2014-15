eecf1b127ea7d9c90557dad81d89aa94d894a23e
Spela Povrzenic <spela.povrzenic@gmail.com>
2014-12-15 16:35:11 +0100

    ZEMLEVID

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,21 +1,22 @@
 # 3. faza: Izdelava zemljevida
 
+#1.ZEMLJEVID____________________________
 # Uvozimo funkcijo za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r")
 
 # Uvozimo zemljevid.
 cat("Uvažam zemljevid...\n")
-obcine <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2/shp/SVN_adm.zip",
+slo <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2/shp/SVN_adm.zip",
                           "slovenija", "SVN_adm1.shp", mapa = "zemljevid",
                           encoding = "Windows-1250")
 
 # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
 preuredi <- function(podatki, zemljevid) {
-  nove.obcine <- c()
-  manjkajo <- ! nove.obcine %in% rownames(podatki)
+  nove.slo <- c()
+  manjkajo <- ! nove.slo %in% rownames(podatki)
   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
   names(M) <- names(podatki)
-  row.names(M) <- nove.obcine[manjkajo]
+  row.names(M) <- nove.slo[manjkajo]
   podatki <- rbind(podatki, M)
   
   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$NAME_1)[rank(zemljevid$NAME_1)]), ]
@@ -28,7 +29,7 @@ preuredi <- function(podatki, zemljevid) {
 }
 
 # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-rregije <- preuredi(regije[-1,], obcine)
+rregije <- preuredi(regije[-1,], slo)
 
 # Izračunamo povprečno velikost družine.
 druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
@@ -40,7 +41,52 @@ cat("Rišem zemljevid...\n")
 pdf("slike/povprecna_druzina.pdf", width=6, height=4)
 
 n = 100
-barve = topo.colors(n)[unlist(1+(n-1)*norm.2002)]
-plot(obcine, col = barve)
+barve =topo.colors(n)[unlist(1+(n-1)*norm.2002)]
+plot(slo, col = barve)
+
+#2.ZEMLJEVID_______________________
+## Uvozimo funkcijo za pobiranje in uvoz zemljevida.
+source("lib/uvozi.zemljevid.r")
+
+# Uvozimo zemljevid.
+cat("Uvažam zemljevid...\n")
+EU <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_subunits.zip",
+                          "slovenija", "SVN_adm1.shp", mapa = "zemljevid",
+                          encoding = "Windows-1250")
+
+# Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
+preuredi <- function(podatki, zemljevid) {
+  nove.EU <- c()
+  manjkajo <- ! nove.EU %in% rownames(podatki)
+  M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
+  names(M) <- names(podatki)
+  row.names(M) <- nove.EU[manjkajo]
+  podatki <- rbind(podatki, M)
+  
+  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$NAME_1)[rank(zemljevid$NAME_1)]), ]
+  if (ncol(podatki) == 1) {
+    out <- data.frame(out)
+    names(out) <- names(podatki)
+    rownames(out) <- rownames(podatki)
+  }
+  return(out)
+}
+
+# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
+rregije <- preuredi(regije[-1,], EU)
+
+# Izračunamo povprečno velikost družine.
+druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
+min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
+max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/povprecna_druzina.pdf", width=6, height=4)
+
+n = 100
+barve =topo.colors(n)[unlist(1+(n-1)*norm.2002)]
+plot(EU, col = barve)
+
 
 dev.off()
