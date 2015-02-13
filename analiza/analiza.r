6c7c4a8e8b894a04756d9c152a639078e39bf1cc
Spela Povrzenic <spela.povrzenic@gmail.com>
2015-02-13 02:02:38 +0100

    analiza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -71,7 +71,7 @@ kvadratna <- lm(donava ~ I(sava^2) + sava)
 curve(predict(kvadratna, data.frame(sava=x)), add = TRUE, col = "red")
 
 ###############################################################################
-
+dev.off()
 
 #PARI
 #save
@@ -80,23 +80,78 @@ pairs(t[,c(3,4,6,8,10,12)])
 pairs(t[,c(2,14,15,17,18,20,22)])
 
 ##################################################
-dev.off()
 
-#EVROPA
-euroana <- apply(podatki.EU , 1, c) #transponiran world
+
+#GRUPIRANJE EVROPE
+library(impute)
 
 podatki.EU <- world[as.character(EU$sovereignt),]
+ma <- match(row.names(podatki.EU), row.names(prebivalci))
+povprecno.prebivalstvo <- apply(prebivalci[ma, c("X1980", "X2020")], 1, mean)
+analiza.eu <- podatki.EU/povprecno.prebivalstvo #podatki deljeno preb
+
+izbor <- analiza.eu[c("X2002", "X2007", "X2013")]
+# dovolimo največ 1 manjkajoč podatek na vrstico
+analiza.izbor <- impute.knn(as.matrix(izbor[apply(is.na(izbor), 1, sum) <= 1,]))$data
+
+hh <- hclust(dist(analiza.izbor))
+plot(hh, hang=0.1, cex=0.7, main = "Evropa",xlab ="Analiza evrope",ylab=NULL,
+     sub = "za leta 2002, 2007, 2013")
+rect.hclust(hh,k=6,border="green")
+
+#####################################
+
+#EVROPA zemljevid
+
+source("lib/uvozi.zemljevid.r")
+EUU <- svet[svet$name_long %in% drzavee,]
+koordinate2 <- coordinates(EUU)
+imena2 <- as.character(EUU$name)
+rownames(koordinate2) <- imena2
+names(imena2) <- imena2
+koordinate2["Norway",1] <- koordinate2["Norway",1] - 2
+koordinate2["Cyprus",2] <- koordinate2["Cyprus",2] - 1
+koordinate2["United Kingdom",1] <- koordinate2["United Kingdom",1]+1
+koordinate2["United Kingdom",2] <- koordinate2["United Kingdom",2]-1
+koordinate2["Sweden",1] <- koordinate2["Sweden",1]-1
+koordinate2["Greece",1] <- koordinate2["Greece",1]-0.8
+koordinate2["Russia",1] <- koordinate2["Russia",1]-65
+koordinate2["Croatia", 2]<- koordinate2["Croatia",2]+0.7
+koordinate2["Croatia", 1]<- koordinate2["Croatia",1]+0.2
+imena2["United Kingdom"] <- "United\nKingdom"
+imena2["Bosnia and Herz."] <- "BiH"
+imena2["Luxembourg"]<- "Lux"
+imena2["Montenegro"]<-"Mont"
+imena2["Macedonia"]<-"Mac"
+imena2["Albania"]<-"Alb"
+imena2["Slovenia"]<-"SLO"
+imena2["Croatia"]<-"HR"
+
+
+analiza.izbor <- scale(analiza.izbor)
+k <- kmeans(analiza.izbor, 6)
+drzavee <- row.names(analiza.izbor)
+m <- match(svet$name_long, drzavee)
+barve <- rainbow(6)
+plot(svet, col=ifelse(is.na(m), "grey", barve[k$cluster[m]]),
+     xlim=c(-22, 40), ylim=c(36, 70))
+
+text(koordinate2,labels=imena2,cex=0.3)
+
+
+
+#k$size koliko je v posamezni skupini držav
+
+
+#################
+#nepomembni podatki zaenkrat
+podatki.EU <- world[as.character(EU$sovereignt),]
+euroana <- apply(podatki.EU , 1, c) #transponiran world
 ma <- match(row.names(podatki.EU), row.names(prebivalci))
 povprecno.prebivalstvo <- apply(prebivalci[m, c("X1980", "X2020")], 1, mean)
 analiza.eu <- podatki.EU/povprecno.prebivalstvo #podatki deljeno preb
 #EU$analiza <- podatki.EU/povprecno.prebivalstvo  #za zemljevid
-
-
-
-
-
-
-
+#m <- match(row.names(eeuro), row.names(prebivalci))
 #colnames(t)[c(2,1)]
 
 ###########################################
